1b1047f9ff3df116a98c9059afa5b8a9654df984
Albert Ji <peleusj@gmail.com>
2023-07-13 14:15:21 +0800

    refactor config structure

---
+++ .config/nvim/plugin/treesitter.lua
@@ -0,0 +1,16 @@
+require("nvim-treesitter.configs").setup {
+    ensure_installed = {
+        "lua", "python", "typescript", "rust", "go"
+    },
+    sync_install = false,
+    highlight = {
+        enable = true,
+    },
+    indent = {
+        enable = false,
+    },
+    context_commentstring = {
+        enable = true,
+        enable_autocmd = false,
+    }
+}
